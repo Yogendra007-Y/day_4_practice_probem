eb19d6a361378c172208cd4d788dc9c6492445c2
Yogendra <dharmayog91@gmail.com>
2021-09-08 19:09:54 +0530

    player who reached 100 has won

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ import java.util.Random;
 
 public class Main {
 
-
         public static final int No_Play = 0;
         public static final int Ladder = 1;
         public static final int Snake = 2;
@@ -14,7 +13,7 @@ public class Main {
             System.out.println("The start position of player is : " + playerposition);
             System.out.println();
             Random ranval = new Random();
-            while (playerposition < 100) {
+            while (playerposition != 100) {
                 int diceroll = ranval.nextInt(6) + 1;
                 int check_play = ranval.nextInt(3);
                 switch (check_play) {
@@ -23,7 +22,7 @@ public class Main {
                     case Ladder:
                         playerposition += diceroll;
                         if (playerposition > 100) {
-                            playerposition = 100;
+                            playerposition -= diceroll;
                         }
                         break;
                     case Snake:
