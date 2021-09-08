f6ef1daa077ddacf0c29289f62f68bf97f104d37
Yogendra <dharmayog91@gmail.com>
2021-09-08 18:55:02 +0530

    checks for an option

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,32 @@
 package com.company;
+import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int playerposition = 0;
-        System.out.println("The position of player is : " + playerposition);
-    }
-}
+        public static final int No_Play = 0;
+        public static final int Ladder = 1;
+        public static final int Snake = 2;
 
+        public static void main(String[] args) {
+
+            int playerposition = 0;
+            System.out.println("The start position of player is : " + playerposition);
+            Random ranval = new Random();
+            int diceroll = ranval.nextInt(6) + 1;
+            int check_play = ranval.nextInt(3);
+            switch (check_play) {
+                case No_Play:
+                    break;
+                case Ladder:
+                    playerposition += diceroll;
+                    break;
+                case Snake:
+                    playerposition -= diceroll;
+                    break;
+            }
+            System.out.println("After the roll dice....The position of player is : " + playerposition);
+
+        }
+
+
+    }
