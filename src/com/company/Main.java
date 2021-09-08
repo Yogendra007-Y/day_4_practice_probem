12ea6ff4455b4afa3e871af5c853301395cf03c3
Yogendra <dharmayog91@gmail.com>
2021-09-08 18:25:53 +0530

    snake_ladder_singleplayer

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int playerposition = 0;
+        System.out.println("The position of player is : " + playerposition);
+    }
+}
+
