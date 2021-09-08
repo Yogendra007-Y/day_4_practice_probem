ff16ab1016b602d0d4d8e3727fa4765ae0112e46
Yogendra <dharmayog91@gmail.com>
2021-09-08 19:03:28 +0530

    to reach winning position 100

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ import java.util.Random;
 
 public class Main {
 
+
         public static final int No_Play = 0;
         public static final int Ladder = 1;
         public static final int Snake = 2;
@@ -11,22 +12,30 @@ public class Main {
 
             int playerposition = 0;
             System.out.println("The start position of player is : " + playerposition);
+            System.out.println();
             Random ranval = new Random();
-            int diceroll = ranval.nextInt(6) + 1;
-            int check_play = ranval.nextInt(3);
-            switch (check_play) {
-                case No_Play:
-                    break;
-                case Ladder:
-                    playerposition += diceroll;
-                    break;
-                case Snake:
-                    playerposition -= diceroll;
-                    break;
+            while (playerposition < 100) {
+                int diceroll = ranval.nextInt(6) + 1;
+                int check_play = ranval.nextInt(3);
+                switch (check_play) {
+                    case No_Play:
+                        break;
+                    case Ladder:
+                        playerposition += diceroll;
+                        if (playerposition > 100) {
+                            playerposition = 100;
+                        }
+                        break;
+                    case Snake:
+                        playerposition -= diceroll;
+                        if (playerposition < 0) {
+                            playerposition = 0;
+                        }
+                        break;
+                }
+                System.out.println("New position is= " + playerposition);
             }
-            System.out.println("After the roll dice....The position of player is : " + playerposition);
+            System.out.println("Player at position " + playerposition + " and also winner");
 
         }
-
-
     }
